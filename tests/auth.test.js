c7266dddcdd65b3d5e1adf7ae39ced1ad97492fd
arl137 <fakhrul_mubarak@telkomsel.co.id>
2024-10-01 00:57:05 +0000

    update tdd

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -4,11 +4,11 @@ const app = require("../index"); // Import your Express app
 describe("POST /register", () => {
   it("should register a new user", async () => {
     const response = await request(app).post("/register").send({
-      name: "Test User",
-      username: "testuser",
-      email: "testuser@gmail.com",
-      password: "testpassword",
-      phoneNumber: "0811223344",
+      name: "Test User 1",
+      username: "testuser1",
+      email: "testuser1@gmail.com",
+      password: "testpassword1",
+      phoneNumber: "08112233441",
       address: "123 Test St",
     });
 
@@ -21,11 +21,11 @@ describe("POST /register", () => {
 
   it("should return error if user already exists", async () => {
     const response = await request(app).post("/register").send({
-      name: "Test User",
-      username: "testuser",
-      email: "testuser@gmail.com", // Same email to check conflict
-      password: "testpassword",
-      phoneNumber: "0811223344",
+      name: "Test User 2",
+      username: "testuser2",
+      email: "testuser2@gmail.com", // Same email to check conflict
+      password: "testpassword2",
+      phoneNumber: "08112233442",
       address: "123 Test St",
     });
 
